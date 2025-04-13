69c2948c9e3e41e7418b83d74e9d3f5be0c7adba
Celestian Hilot <dabberdoodle1@gmail.com>
2025-04-13 10:43:43 +0800

    Undid changes

--- build_windows/CMakeFiles/copy_res.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_res"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_res.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
